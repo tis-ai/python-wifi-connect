bb7f642ba4ddcc70e738d91bc0de68c7fef5364f
MoriKeisuke <mori.keisuke@tis.co.jp>
2019-06-07 14:10:27 +0900

    fix(run path): 実行ファイルのパスを修正

--- setting.sh
+++ setting.sh
@@ -2,4 +2,4 @@
 
 export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
 echo "Setting up wifi connection. Connect to the PFC_EDU-${RESIN_DEVICE_NAME_AT_INIT} access point"
-./scripts/run.sh
+./run.sh
